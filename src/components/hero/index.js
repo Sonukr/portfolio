b14f0bd22e921eeff895ccbf4fea2a775890da02
Sonukr <sonuyadav9756@gmail.com>
2019-05-08 21:52:30 +0530

    social link added

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -29,6 +29,23 @@ export class Hero extends Component {
                             <p className={styles.subtitle}>
                               <b>Front-end/UI Develoepr </b> from India.
                             </p>
+                            <div className={styles.linksWrapper}>
+                              <a href="https://github.com/sonukr" target="_blank" class={classnames(styles.linkItem, styles.github)} title="Github">
+                                <i class="fab fa-github"></i>
+                              </a>
+                              <a href="https://stackoverflow.com/users/4223801/sonu" target="_blank" class={classnames(styles.linkItem, styles.stackoverflow)} title="overflow">
+                                <i class="fab fa-stack-overflow"></i>
+                              </a>
+                              <a href="http://codepen.io/sonukry/" target="_blank" class={classnames(styles.linkItem, styles.codepan)} title="Codepan">
+                                <i class="fab fa-codepen"></i>
+                              </a>
+                              <a href="https://in.linkedin.com/in/krsonu" target="_blank" class={classnames(styles.linkItem, styles.linkedin)} title="linkedin">
+                                <i class="fab fa-linkedin"></i>
+                              </a>
+                              <a href="https://www.facebook.com/sonukry" target="_blank" class={classnames(styles.linkItem, styles.facebook)} title="Facebook">
+                                <i class="fab fa-facebook-square"></i>
+                              </a>
+                            </div>
                           </div>
                         </div>
                         <div className="col-md-6 col-sm-6 col-xs-6 text-right">
